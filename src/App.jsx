0e96269c2b7e0df04e238d513ef0bcef19b8b933
ChannimEY <channimey1@gmail.com>
2025-03-03 22:01:52 +0700

    add: discountpage com

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import "./App.css";
-import DiscountPage from "./components/DiscountPageCom/DiscountPage";
 import AppRoutes from "./routes/AppRoutes";
 function App() {
    return <AppRoutes />;
-   // <DiscountPage/>
+
 }
 
 export default App;
-
