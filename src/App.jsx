6469819cf64379e61b5bd7020e089ebf593990f0
Raksa <novraksa204@gmail.com>
2025-03-04 10:17:42 +0700

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import "./App.css";
+// import "./App.css";
+// import AppRoutes from "./AppRoutes";
+// function App() {
+//    return <AppRoutes />;
 
-function App() {
-   return <AppRoutes />;
+// }
 
+// export default App;
+
+
+
+import React from "react";
+import AppRoutes from "./routes/AppRoutes";
+import FilterCom from "./components/DiscountPageCom/FilterCom.jsx";
+import DiscountPage from "./components/DiscountPageCom/DiscountPage.jsx";
+// import FilterCom from "./components/FilterCom";
+function App(){
+   return (
+      // <AppRoutes />
+      //  <FilterCom/>
+       <DiscountPage/>
+   );
 }
-
 export default App;
-
