bf5c8bfb10672a9ef2f4ae330401c69a1fe6167a
vichekaSokhem <vichekasokhem4@gmail.com>
2025-03-04 19:38:26 +0700

    add new arrivals product and discount product

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import HomeBanner from "../components/home-components/HomeBanner"
 import AssuranceSection from "../components/home-components/AssuranceSection";
 import PopularCategories from "../components/home-components/PopularCategories"
+import DiscountProduct from "../components/home-components/DiscountProduct"
 import NewArrivals from "../components/home-components/NewArrivals";
 
 
@@ -10,15 +11,17 @@ export default function Home() {
   <div className="pt-40">
   <HomeBanner/>
   </div>
-  <div className="pt-14">
+  <div className="pt-10">
   <AssuranceSection/>
   </div>
-  <div className="pt-14">
+  <div className="pt-10">
   <PopularCategories/>
   </div>
-  <div>
+  <div className="pt-10">
     <NewArrivals/>
   </div>
-
+  <div className="pt-10">
+    <DiscountProduct/>
+  </div>
   </main>
 }
