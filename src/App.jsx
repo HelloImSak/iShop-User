08341d8aad004f207da1664e45c02f1784e06210
John Doe <choeunsela332@gmail.com>
2025-03-07 07:02:49 +0700

    add:update cateProduct

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import "./App.css";
-import AppRoutes from "./routes/AppRoutes";
+
+
+
 function App() {
-  return <AppRoutes />;
+  //   return <AppRoutes />;
+  return <AppRoutes/>
 }
 
 export default App;
