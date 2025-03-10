a43bc887039f07deb3cca45087ef49afc0ebbf97
John Doe <choeunsela332@gmail.com>
2025-03-10 23:38:29 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import "./App.css";
+import ProductPhone from "./components/ProductPhoneCom/ProductPhone";
+
 import AppRoutes from "./routes/AppRoutes";
 function App() {
-  return <AppRoutes />;
+  // return <AppRoutes />;
+  return <ProductPhone/>
 }
 
 export default App;
--- src/components/ProductPhoneCom/ProductPhone.jsx
+++ src/components/ProductPhoneCom/ProductPhone.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
-import BannerPhone from './BannerPhone';
+import BannerPh from './BannerPh';
 import FilterCom from '../DiscountPageCom/FilterCom';
-
-
-
-export default function LinkPhone() {
+export default function ProductPhone() {
     const [price, setPrice] = useState(4950);
     const { data: brandData, isLoading, isError } = useGetAllBrandQuery();
   return (
    <main>
-    <BannerPhone/>
-    <FilterCom/>
+   <BannerPh/>
+   <FilterCom/>
+  
        <div className="container mx-auto pt-[32px] px-[100px]">
            <h1 className="flex justify-center text-2xl md:text-3xl font-semibold mb-4 mt-8 text-primary">Top Category</h1>
            {/* section1 */}
