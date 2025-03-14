99d0a7e60c700f067ef6ae6b72c1f49271136d54
ChannimEY <channimey1@gmail.com>
2025-03-15 02:37:04 +0700

    add: update responsive

--- src/components/AllProductCom/BannerAllPro.jsx
+++ src/components/AllProductCom/BannerAllPro.jsx
@@ -5,7 +5,7 @@ export default function BannerAllPro() {
     <section className="flex justify-center py-10 mx-[50px] sm:mx-[20px] ">
       <div className="w-full rounded-3xl bg-[#E2DEDB] p-10 flex flex-row items-center gap-6">
         {/* Left Section - Text */}
-        <div className="w-[40%] ">
+        <div className="w-[40%]">
           <h2 className="2xl:text-4xl xl:text-3xl lg:text-3xl md:text-2xl sm:text-[16px] font-semibold text-gray-700">
             You're choose the{" "}
             <span className="text-orange-500 font-bold">Right</span> place to buy your favorite thing.
@@ -20,7 +20,7 @@ export default function BannerAllPro() {
           <img
               src="https://cdn.builder.io/api/v1/image/assets/TEMP/9abc4629e1f0fc8ace9ddce348de792b87d2f7e0bd14a9b5fa5c16c7fa600e76?placeholderIfAbsent=true&apiKey=5cd3de3f08094ca3afe2694744931c58"
             alt="Apple iMacs"
-            className="w-full sm-w-[300px]"
+            className="w-full"
           />
         </div>
       </div>
